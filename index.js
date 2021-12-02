2574a6f9c156756a73add3e5e936ede8186150b3
+Michael Kolb <+michael.w.kolb@gmail.com>
2021-12-02 15:19:00 -0500

    commit tree 6a8009dec58f6d71e31f43f50a0f28ac89caccaa

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function superbowlWin(array) {
+    let foundWin = array.find(function(element) {
+        return element.result === 'W';
+    })
+    if (foundWin) {
+        console.log(foundWin.year);
+        return foundWin.year;
+    }
+    else {
+        return foundWin;
+    }
+}
